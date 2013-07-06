2aa5fc6196ab1d92ff70f8acd31fc3d651ee41d9
Michal Jedynak <m.jedynak@gmail.com>
2013-07-06 22:36:31 +0200

    reading file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
+var buffer = fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World3!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
